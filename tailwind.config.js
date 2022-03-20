78e90b689f6531dea0c0f99e57df464bcb6a558f
Tiago <tiago@Tiagos-MacBook-Air.local>
2022-03-20 17:06:35 -0300

    add custom colors and custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      orange: 'hsl(25, 97%, 53%)',
+      white: 'hsl(0, 0%, 100%)',
+      'light-grey': 'hsl(217, 12%, 63%)',
+      'medium-grey': 'hsl(216, 12%, 54%)',
+      'dark-blue': 'hsl(213, 19%, 18%)',
+      'very-dark-blue': 'hsl(216, 12%, 8%)',
+    },
     extend: {},
   },
   plugins: [],
